ea6e47a765fc37999d89897ff22e5e888709c92a
MedELHattab <136116042+MedELHattab@users.noreply.github.com>
2025-01-02 11:39:07 +0100

    i fixed bug in user management component

--- src/app/auth.utils.ts
+++ src/app/auth.utils.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
 
 export function getAuthHeaders(): HttpHeaders {
-  const token = localStorage.getItem('authToken'); // Retrieve the token from local storage
+  const token = localStorage.getItem('token'); // Retrieve the token from local storage
   return new HttpHeaders({
     'Content-Type': 'application/json',
     Authorization: `Bearer ${token}` // Add the token to the Authorization header
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -26,18 +26,21 @@ export class UserService {
 
   // POST: Create a new user
   createUser(user: any): Observable<any> {
-    return this.http.post(this.apiUrl, user);
+    const headers = getAuthHeaders();
+    return this.http.post(this.apiUrl, user,  { headers });
   }
 
   // PUT: Update an existing user
   updateUser(id: number, user: any): Observable<any> {
+    const headers = getAuthHeaders();
     const url = `${this.apiUrl}/${id}`;
-    return this.http.put(url, user);
+    return this.http.put(url, user, { headers });
   }
 
   // DELETE: Delete a user by ID
   deleteUser(id: number): Observable<any> {
     const url = `${this.apiUrl}/${id}`;
-    return this.http.delete(url);
+    const headers = getAuthHeaders();
+    return this.http.delete(url, {headers});
   }
 }
